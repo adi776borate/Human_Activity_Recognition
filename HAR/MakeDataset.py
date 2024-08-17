dff8e21d1737d8cc0e9c32d5a662768ff46b9454
Aditya Borate <23110065@iitgn.ac.in>
2024-08-17 17:27:18 +0530

    Included __name__ == __'main'__ in MakeDataset.py

--- HAR/MakeDataset.py
+++ HAR/MakeDataset.py
@@ -78,8 +78,9 @@ y = np.concatenate((y_train,y_test))
 seed = 4
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=seed,stratify=y)
 
-print("Training data shape: ",X_train.shape)
-print("Testing data shape: ",X_test.shape)
+if __name__ == "__main__":
+    print("Training data shape: ",X_train.shape)
+    print("Testing data shape: ",X_test.shape)
 
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
